cb4cc82242ebd4d74d4b622c16495d351cc0944f
Javier Galarza <jegj57@gmail.com>
2019-07-22 10:39:07 -0500

    Add basic files for a empty server

---
+++ index.js
@@ -0,0 +1,42 @@
+/* index.js */
+
+require('dotenv').config();
+require('app-module-path').addPath(__dirname);
+
+global.E = require('@rebelstack-io/expressif');
+// global.db = new E.DB();
+global.LOGGER = new E.BasicLogger();
+
+const server = new E.Server(
+	{
+		"port": process.env.PORT,
+		"routers":[
+			{ "relpath": "routers" }
+		]
+	}
+);
+const auth = new E.Auth(process.env.JWT_SECRET);
+
+// Unhandled exceptions
+process.on('uncaughtException', function (err) {
+	LOGGER.error('Unexpected Error:', err, err.stack);
+	process.exit(1);
+});
+
+// Graceful-shutdown
+process.on('SIGINT', () => {
+	LOGGER.info('SIGINT signal received.');
+	try {
+		server.close()
+		// Check db connection and close it
+		if (global.db) {
+			global.db.close();
+		}
+	} catch (error) {
+		LOGGER.error(error);
+		process.exit(1);
+	}
+});
+
+
+server.start();
